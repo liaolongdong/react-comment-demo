30ec36b7a97fda8e364dbb4167b16a831b7a027d
liaolongdong <924902324@qq.com>
2017-06-29 23:59:37 +0800

    first commit

---
+++ src/CommentList.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+// import Comment from './Comment';
+
+class CommentList extends Component {
+	static defaultProps = {
+		comments: []
+	}
+	render() {
+		// const comments = [
+		// 	{username: 'xiaoxin', content: 'hello'},
+		// 	{username: 'xiaonian', content: 'react'},
+		// 	{username: 'xiaowu', content: 'demo'}
+		// ];
+
+		// return (
+		// 	<div>
+		// 		{comments.map((comment, i) => {
+		// 			return (
+		// 				<div key={i}>
+		// 					{comment.username}：{comment.content}
+		// 				</div>
+		// 			)
+		// 		})}
+		// 	</div>
+		// )
+		
+		return (
+			<div>
+				{this.props.comments.map((comment, i) => <Comment comment={comment} key={i}/>)}
+			</div>
+		)
+	}
+}
+
+export default CommentList
---
+++ src/index.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import CommentApp from './CommentApp';
+import './index.css';
+
+ReactDOM.render(
+	<CommentApp/>,
+	document.getElementById('root')
+);
