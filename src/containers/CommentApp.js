1dea52cdf3a1daac80a5efadda1f5b6d899bfdcb
liaolongdong <924902324@qq.com>
2017-09-29 17:41:53 +0800

    增加通用函数方法文件

--- src/containers/CommentApp.js
+++ src/containers/CommentApp.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
+import {formatPhoneNum} from '../commonTool';
 
 export default class CommentApp extends Component {
+	componentDidMount () {
+		// console.log(formatPhoneNum('135****1025'));
+	}
 	render() {
 		return (
 			<div className='wrapper'>
